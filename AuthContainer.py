a5e2a449a2ec95517a81601ec3b22b03e4432ea9
Christian Stankowic <webmaster@christian-stankowic.de>
2017-03-16 15:10:07 +0100

    Auth container and code clean-up - started to work on revert functionality for katprep_maintenance - implemented auth container class for easier authentication credentials provisiong - cleaned-up code

---
+++ AuthContainer.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Class for managing multiple authentication credentials
+"""
+
+import os
+import stat
+import json
+from urlparse import urlparse
+
+
+
+class AuthContainer:
+    """
+.. class:: AuthContainer
+    """
+    FILENAME = ""
+    """
+    str: correspending file
+    """
+    CREDENTIALS = {}
+    """
+    dict: authentication credentials
+    """
+
+    def __init__(self, filename):
+        """
+        Constructor, creating the class. It requires specifying a filename.
+        If the file already exists, already existing entries are imported.
+
+        :param filename: filename
+        :type filename: str
+        """
+        self.FILENAME = filename
+        try:
+            self.__import()
+        except ValueError:
+            pass
+
+
+
+    def __import(self):
+        """This function imports definitions from the file."""
+        global CREDENTIALS
+
+        if os.path.exists(self.FILENAME) and \
+            oct(stat.S_IMODE(os.lstat(self.FILENAME).st_mode)) == "0600":
+            #loading file
+            self.CREDENTIALS = json.loads(self.get_json(self.FILENAME))
+        else:
+            raise ValueError("File non-existent or file mode not 0600!")
+
+
+
+    def get_json(self, filename):
+        """
+        Reads a JSON file and returns the whole content as one-liner.
+
+        :param filename: the JSON filename
+        :type filename: str
+        """
+        try:
+            with open(filename, "r") as json_file:
+                json_data = json_file.read().replace("\n", "")
+            return json_data
+        except IOError as err:
+            LOGGER.error("Unable to read file '{}': '{}'".format(filename, err))
+
+
+    def save(self):
+        """
+        This function stores the changed authentication container to disk.
+        """
+        try:
+            target = open(self.FILENAME, 'w')
+            target.write(
+                json.dumps(self.CREDENTIALS)
+            )
+            target.close()
+            #setting the good perms
+            os.chmod(self.FILENAME, 0600)
+        except IOError as err:
+            raise err
+
+
+
+    def __manage_credentials(self, hostname, username, password,
+        remove_entry=False):
+        """
+        This functions adds or removes credentials to/from the authentication
+        container.
+        Adding credentials requires a hostname, username and corresponding
+        password. Removing credentials only requires a hostname.
+
+        There are two alias functions for credentials management:
+        add_credentials() and remove_credentials()
+
+        :param hostname: hostname
+        :type hostname: str
+        :param username: username
+        :type username: str
+        :param password: corresponding password
+        :type password: str
+        :param remove_entry: setting True will remove an entry
+        :type remove_entry: bool
+        """
+        global CREDENTIALS
+        hostname = self.cut_hostname(hostname)
+
+        try:
+            if remove_entry:
+                #remove entry
+                del self.CREDENTIALS[hostname]
+            else:
+                #add entry
+                self.CREDENTIALS[hostname] = {}
+                self.CREDENTIALS[hostname]["username"] = username
+                self.CREDENTIALS[hostname]["password"] = password
+        except KeyError:
+            pass
+
+    #aliases
+    def add_credentials(self, hostname, username, password):
+        """
+        Adds credentials to the authentication container.
+
+        :param hostname: hostname
+        :type hostname: str
+        :param username: username
+        :type username: str
+        :param password: corresponding password
+        :type password: str
+        """
+        return self.__manage_credentials(hostname, username, password)
+
+    def remove_credentials(self, hostname):
+        """
+        Removes credentials from the authentication container.
+
+        :param hostname: hostname
+        :type hostname: str
+        """
+        return self.__manage_credentials(hostname, "", "", True)
+
+
+
+    def get_hostnames(self):
+        """This function returns hostnames"""
+        return self.CREDENTIALS.keys()
+
+
+
+    @staticmethod
+    def cut_hostname(snippet):
+        """
+        This function removes protocol pre- and postfix data in order to
+        find/create generic authentication information.
+
+        :param snippet: connection string, URI/URL,...
+        :type snippet: str
+        """
+        parsed_uri = urlparse(snippet)
+        host = '{uri.netloc}'.format(uri=parsed_uri)
+        if host == "":
+            #non-URL/URI
+            host = snippet
+        return host
+
+
+
+    def get_credential(self, hostname):
+        """
+        This function returns credentials for a particular hostname.
+        
+        :param hostname: hostname
+        :type hostname: str
+        """
+        hostname = self.cut_hostname(hostname)
+        try:
+            return (
+                self.CREDENTIALS[hostname]["username"],
+                self.CREDENTIALS[hostname]["password"]
+                )
+        except KeyError:
+            pass
---
+++ katprep_authconfig.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+A script which maintains entries in a authentication container.
+"""
+
+import argparse
+import logging
+import json
+import getpass
+from AuthContainer import AuthContainer
+
+__version__ = "0.0.1"
+LOGGER = logging.getLogger('katprep_authconfig')
+"""
+logging: Logger instance
+"""
+CONTAINER = None
+"""
+AuthContainer: authentication container file
+"""
+
+
+
+def list(args):
+    """
+    This function lists entries from the authentication container.
+    """
+    for hostname in CONTAINER.get_hostnames():
+        #get credentials
+        credentials = CONTAINER.get_credential(hostname)
+        if options.show_passwords:
+            password = credentials[1]
+        else:
+            password = "xxx"
+        #print entry
+        print "{} (Username: {} / Password: {})".format(
+            hostname, credentials[0], password
+        )
+
+
+
+def add(args):
+    """
+    This function adds/modifies an entry to/from the authentication container.
+    """
+    while options.entry_hostname == "":
+        #prompt for hostname
+        options.entry_hostname = raw_input("Hostname: ")
+    while options.entry_username == "":
+        #prompt for hostname
+        options.entry_username = raw_input(
+            "{} Username: ".format(options.entry_hostname)
+        )
+    while options.entry_password == "":
+        #prompt for password
+        options.entry_password = getpass.getpass(
+            "{} Password: ".format(options.entry_hostname)
+        )
+    LOGGER.debug("Adding entry hostname='{}', username='{}'...".format(
+        options.entry_hostname, options.entry_username)
+    )
+    CONTAINER.add_credentials(
+        options.entry_hostname, options.entry_username, options.entry_password
+    )
+    CONTAINER.save()
+
+
+
+def remove(args):
+    """
+    This function removes an entry from the authentication container.
+    """
+    while options.entry_hostname == "":
+        #prompt for hostname
+        options.entry_hostname = raw_input("Hostname: ")
+    LOGGER.debug("Removing entry hostname='{}'...".format(
+        options.entry_hostname)
+    )
+    CONTAINER.remove_credentials(options.entry_hostname)
+    CONTAINER.save()
+
+
+
+def parse_options(args=None):
+    """Parses options and arguments."""
+    desc = '''katprep_authconfig.py is used for creating, modifying and
+    removing entries in/from an authentication container.
+    Authentication containers include various authentication credentials for
+    external systems that can be accessed from the katprep utilities (e.g.
+    monitoring systems, hypervisor connections, etc.).
+    This will make system maintenance automation easier as you don't have to
+    enter credentials every time.
+    '''
+    epilog = '''Check-out the website for more details:
+     http://github.com/stdevel/katprep'''
+    parser = argparse.ArgumentParser(
+        description=desc, version=__version__, epilog=epilog
+    )
+
+    #define option groups
+    gen_opts = parser.add_argument_group("generic arguments")
+
+    #GENERIC ARGUMENTS
+    #-q / --quiet
+    gen_opts.add_argument("-q", "--quiet", action="store_true", \
+    dest="generic_quiet", \
+    default=False, help="don't print status messages to stdout (default: no)")
+    #-d / --debug
+    gen_opts.add_argument("-d", "--debug", dest="generic_debug", \
+    default=False, action="store_true", \
+    help="enable debugging outputs (default: no)")
+    #authentication container
+    gen_opts.add_argument('container', metavar='FILE', nargs=1, \
+    help='An authentication container', type=str)
+
+    #COMMANDS
+    subparsers = parser.add_subparsers(title='commands', \
+    description='controlling maintenance stages', help='additional help')
+    cmd_list = subparsers.add_parser("list", help="listing entries")
+    cmd_list.add_argument("-a", "--show-passwords", action="store_true", \
+    dest="show_passwords", default=False, help="also shows passwords " \
+    "(default: no)")
+    cmd_list.set_defaults(func=list)
+
+    cmd_add = subparsers.add_parser("add", help="adding/modifying entries")
+    cmd_add.set_defaults(func=add)
+    cmd_add.add_argument("-H", "--hostname", action="store", default="", \
+    dest="entry_hostname", metavar="HOSTNAME", help="hostname entry")
+    cmd_add.add_argument("-u", "--username", action="store", default="", \
+    dest="entry_username", metavar="USERNAME", help="username")
+    cmd_add.add_argument("-p", "--password", action="store", default="", \
+    dest="entry_password", metavar="PASSWORD", help="corresponding password")
+
+    cmd_remove = subparsers.add_parser("remove", help="removing entries")
+    cmd_remove.add_argument("-H", "--hostname", action="store", default="", \
+    dest="entry_hostname", metavar="HOSTNAME", help="hostname entry")
+    cmd_remove.set_defaults(func=remove)
+
+    #parse options and arguments
+    options = parser.parse_args()
+    return (options, args)
+
+
+
+def main(options, args):
+    """Main function, starts the logic based on parameters."""
+    global CONTAINER
+
+    LOGGER.debug("Options: {0}".format(options))
+    LOGGER.debug("Arguments: {0}".format(args))
+
+    #load container
+    CONTAINER = AuthContainer(options.container[0])
+
+    #start action
+    options.func(options.func)
+
+
+
+if __name__ == "__main__":
+    (options, args) = parse_options()
+
+    #set logging level
+    logging.basicConfig()
+    if options.generic_debug:
+        LOGGER.setLevel(logging.DEBUG)
+    elif options.generic_quiet:
+        LOGGER.setLevel(logging.ERROR)
+    else:
+        LOGGER.setLevel(logging.INFO)
+
+    main(options, args)
