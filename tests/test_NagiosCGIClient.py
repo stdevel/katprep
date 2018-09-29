d8b2a65d7ebfb03f11ab018ef0cab1346eea39af
Niko Wenselowski <der@nik0.de>
2018-09-29 22:15:01 +0200

    Migrated to pytest.

--- tests/test_NagiosCGIClient.py
+++ tests/test_NagiosCGIClient.py
@@ -6,157 +6,97 @@ Unit tests for Nagios/Icinga 1.x CGI integration
 
 import os
 import time
-import unittest
 import logging
 import json
+import pytest
 from katprep.clients.NagiosCGIClient import NagiosCGIClient
 from katprep.clients import SessionException, UnsupportedRequestException
 
-class NagiosCGIClientTest(unittest.TestCase):
-    """
-    NagiosCGIClient test cases
-    """
-    LOGGER = logging.getLogger('NagiosCGIClientTest')
-    """
-    logging: Logger instance
-    """
-    cgi_nagios = None
-    """
-    NagiosCGIClient: Nagios CGI client (for legacy)
-    """
-    config = None
-    """
-    str: JSON object containing valid hosts and services
-    """
-    set_up = False
-    """
-    bool: Flag whether the connection was set up
-    """
+
+@pytest.fixture(scope='session')
+def config():
+    config_file = "nagios_config.json"
+    if not os.path.isfile(config_file):
+        pytest.skip("Please create configuration file %s!" % config_file)
+
+    try:
+        with open(config_file, "r") as json_file:
+            return json.load(json_file)
+    except IOError as err:
+        pytest.skip("Unable to read configuration file: '%s'", err)
 
 
-
-    def setUp(self):
-        """
-        Connecting the interfaces
-        """
-        #only set-up _all_ the stuff once
-        if not self.set_up:
-            #instance logging
-            logging.basicConfig()
-            self.LOGGER.setLevel(logging.DEBUG)
-            #reading configuration
-            try:
-                with open("nagios_config.json", "r") as json_file:
-                    json_data = json_file.read().replace("\n", "")
-                self.config = json.loads(json_data)
-            except IOError as err:
-                self.LOGGER.error(
-                    "Unable to read configuration file: '%s'", err
-                )
-            #Legacy
-            self.cgi_nagios = NagiosCGIClient(
-                logging.ERROR, self.config["legacy"]["hostname"],
-                self.config["legacy"]["cgi_user"],
-                self.config["legacy"]["cgi_pass"],
-                verify=False
-            )
-            self.set_up = True
-
-
-
-    def tearDown(self):
-        """
-        Function that is executed after every test
-        """
-        #wait for changes to be applied
+@pytest.fixture
+def nagiosClient(config):
+    try:
+        yield NagiosCGIClient(
+            logging.ERROR,
+            config["legacy"]["hostname"],
+            config["legacy"]["cgi_user"],
+            config["legacy"]["cgi_pass"],
+            verify=False
+        )
+    finally:
         time.sleep(30)
 
 
-
-    def test_valid_login(self):
-        """
-        Ensure exceptions on valid logins
-        """
-        #dummy call
-        self.cgi_nagios.dummy_call()
-
-    def test_invalid_login(self):
-        """
-        Ensure exceptions on invalid logins
-        """
-        with self.assertRaises(SessionException):
-            cgi_dummy = NagiosCGIClient(
-                logging.ERROR, self.config["legacy"]["hostname"],
-                "giertz", "paulapinkepank",
-                verify=False
-            )
-            #dummy call
-            cgi_dummy.dummy_call()
+def test_valid_login(nagiosClient):
+    """
+    Ensure exceptions on valid logins
+    """
+    nagiosClient.dummy_call()
 
 
-
-    def test_schedule_downtime_host(self):
-        """
-        Ensure that downtimes can be scheduled, even on ancient systems
-        """
-        #schedule downtime
-        self.cgi_nagios.schedule_downtime(
-            self.config["legacy"]["host"], "host"
+def test_invalid_login(config):
+    """
+    Ensure exceptions on invalid logins
+    """
+    with pytest.raises(SessionException):
+        client = NagiosCGIClient(
+            logging.ERROR,
+            config["legacy"]["hostname"],
+            "giertz",
+            "paulapinkepank",
+            verify=False
         )
-
-    def test_has_downtime(self):
-        """
-        Ensure that checking downtime is working
-        """
-        self.assertTrue(
-            self.cgi_nagios.has_downtime(
-                self.config["legacy"]["host"]
-            )
-        )
+        client.dummy_call()
 
 
+def test_scheduling_downtime_for_host(nagiosClient, config):
+    """
+    Testing downtime scheduling.
 
-    def test_unsupported_request(self):
-        """
-        Ensure unsupported calls on Nagios will die in a fire
-        """
-        with self.assertRaises(UnsupportedRequestException):
-            #try to remove downtime
-            self.cgi_nagios.remove_downtime("dummy")
-
-
-
-    def test_get_hosts(self):
-        """
-        Ensure that receiving hosts is possible
-        """
-        hosts = self.cgi_nagios.get_hosts()
-        self.assertTrue(
-            self.config["legacy"]["host"] in str(hosts)
-        )
+    Ensure that downtimes can be scheduled, even on ancient systems.
+    Ensure that checking downtime is working.
+    """
+    host = config["legacy"]["host"]
+    nagiosClient.schedule_downtime(host, "host")
+    assert nagiosClient.has_downtime(host)
 
 
-
-    def test_get_services(self):
-        """
-        Ensure that hosts include existing services
-        """
-        services = self.cgi_nagios.get_services(
-            self.config["legacy"]["host"], only_failed=False
-        )
-        self.assertTrue(
-            self.config["legacy"]["host_service"] in str(services) and \
-            len(services) == self.config["legacy"]["host_services"]
-        )
+def test_unsupported_request(nagiosClient):
+    """
+    Ensure unsupported calls on Nagios will die in a fire
+    """
+    with pytest.raises(UnsupportedRequestException):
+        # try to remove downtime
+        nagiosClient.remove_downtime("dummy")
 
 
+def test_get_hosts(nagiosClient, config):
+    """
+    Ensure that receiving hosts is possible
+    """
+    hosts = nagiosClient.get_hosts()
+    assert config["legacy"]["host"] in str(hosts)
 
-if __name__ == "__main__":
-    #start tests or die in a fire
-    if not os.path.isfile("nagios_config.json"):
-        print "Please create configuration file nagios_config.json!"
-        exit(1)
-    else:
-        #do not sort test cases as there are dependencies
-        unittest.sortTestMethodsUsing = None
-        unittest.main()
+
+def test_get_services(nagiosClient, config):
+    """
+    Ensure that hosts include existing services
+    """
+    services = nagiosClient.get_services(
+        config["legacy"]["host"], only_failed=False
+    )
+    assert config["legacy"]["host_service"] in str(services)
+    assert len(services) == config["legacy"]["host_services"]
