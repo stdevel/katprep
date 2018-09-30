0ee8e540d36f28bf6e95dcc9c468e4cacac48dec
Niko Wenselowski <der@nik0.de>
2018-09-30 11:55:44 +0200

    Coming to the future with pytest.

--- tests/test_ForemanAPIClient.py
+++ tests/test_ForemanAPIClient.py
@@ -4,144 +4,39 @@
 Unit tests for Foreman API integration
 """
 
-import os
-import unittest
-import logging
+from __future__ import absolute_import
+
 import json
+import logging
+import pytest
 import random
 from katprep.clients.ForemanAPIClient import ForemanAPIClient
 from katprep.clients import SessionException
 
-class ForemanAPIClientTest(unittest.TestCase):
-    """
-    ForemanAPIClient test cases
-    """
-    LOGGER = logging.getLogger('ForemanAPIClientTest')
-    """
-    logging: Logger instance
-    """
-    api_foreman = None
-    """
-    ForemanAPIClient: Foreman API client
-    """
-    config = None
-    """
-    str: JSON object containing valid hostgroup, location, organization,
-    environment and host objects
-    """
-    bookmark_id = 0
-    """
-    int: ID of temporary bookmark
-    """
-    set_up = False
-    """
-    bool: Flag whether the connection was set up
-    """
+from .utilities import load_config
 
 
-
-    def setUp(self):
-        """
-        Connecting the interface and populating demo content
-        """
-        #only set-up _all_ the stuff once
-        if not self.set_up:
-            #instance logging
-            logging.basicConfig()
-            self.LOGGER.setLevel(logging.DEBUG)
-            #reading configuration
-            try:
-                with open("fman_config.json", "r") as json_file:
-                    json_data = json_file.read().replace("\n", "")
-                self.config = json.loads(json_data)
-            except IOError as err:
-                self.LOGGER.error(
-                    "Unable to read configuration file: '%s'", err
-                )
-            #connect to API
-            self.api_foreman = ForemanAPIClient(
-                logging.ERROR, self.config["config"]["hostname"],
-                self.config["config"]["api_user"],
-                self.config["config"]["api_pass"],
-                verify=False
-            )
-            #create demo bookmark
-            try:
-                self.api_foreman.api_post(
-                    "/bookmarks", '''
-                        {
-                            "bookmark":
-                            {
-                                "name": "ForemanAPIClientTest",
-                                "controller": "dashboard",
-                                "query": "architecture = x86_64",
-                                "public": true
-                            }
-                        }'''
-                )
-            except SessionException as err:
-                if "422" in err:
-                    #demo content already present
-                    pass
-            #Retrieving the ID of the previously created bookmark
-            bookmarks = json.loads(
-                self.api_foreman.api_get(
-                    "/bookmarks"
-                )
-            )
-            for bookmark in bookmarks["results"]:
-                if bookmark["name"] == "ForemanAPIClientTest":
-                    self.bookmark_id = bookmark["id"]
-        self.set_up = True
-
-    def tearDown(self):
-        """
-        Ensure to remove demo content if not removed, yet
-        """
-        try:
-            self.api_foreman.api_delete(
-                "/bookmarks/{}".format(self.bookmark_id), ""
-            )
-        except SessionException as err:
-            if "404" in err:
-                #demo content already removed
-                pass
+@pytest.fixture
+def config():
+    return load_config("fman_config.json")
 
 
-
-    def test_valid_login(self):
-        """
-        Ensure exceptions on valid logins
-        """
-        #dummy call
-        self.api_foreman.api_get("/architectures")
-
-    def invalid_login(self):
-        """
-        Ensure exceptions on invalid logins
-        """
-        with self.assertRaises(SessionException):
-            api_dummy = ForemanAPIClient(
-                logging.ERROR, self.config["config"]["hostname"],
-                "giertz", "paulapinkepank",
-                verify=False
-            )
-            #dummy call
-            api_dummy.api_get("/status")
+@pytest.fixture
+def client(config):
+    return ForemanAPIClient(
+        logging.ERROR,
+        config["config"]["hostname"],
+        config["config"]["api_user"],
+        config["config"]["api_pass"],
+        verify=False
+    )
 
 
-
-    #def test_deny_legacy(self):
-
-
-
-    #TODO: remove after Spacewalk support (mark as private function)
-    def api_post(self):
-        """
-        Ensure that GET calls are working
-        """
-        #Creating a dummy bookmark
-        self.api_foreman.api_post(
+@pytest.fixture
+def bookmark_id(client):
+    # create demo bookmark
+    try:
+        client.api_post(
             "/bookmarks", '''
                 {
                     "bookmark":
@@ -153,134 +48,143 @@ class ForemanAPIClientTest(unittest.TestCase):
                     }
                 }'''
         )
+    except SessionException as err:
+        if "422" not in err:
+            # 422 demo content already present
+            # We have a problem if it is something different
+            raise
 
-    def test_api_get(self):
-        """
-        Ensure that GET calls are working
-        """
-        bookmarks = json.loads(
-            self.api_foreman.api_get(
-                "/bookmarks/{}".format(self.bookmark_id)
-            )
-        )
-        self.assertTrue(
-            bool(str(bookmarks) != "")
-        )
-
-    def test_api_put(self):
-        """
-        Ensure that PUT calls are working
-        """
-        self.api_foreman.api_put(
-            "/bookmarks/{}".format(self.bookmark_id), '''
-                {
-                    "bookmark": {
-                        "name": "ForemanAPIClientTest",
-                        "controller": "dashboard",
-                        "query": "architecture = i386"
-                    }
-                }'''
-        )
-
-    def test_api_delete(self):
-        """
-        Ensure that DELETE calls are working
-        """
-        self.api_foreman.api_delete(
-            "/bookmarks/{}".format(self.bookmark_id), ""
-        )
-
-    def test_api_invalid(self):
-        """
-        Ensure that dumbass calls will fail
-        """
-        with self.assertRaises(SessionException):
-            self.api_foreman.api_get("/giertz/stdevel")
-
-
-
-    def test_get_name_by_id(self):
-        """
-        Ensure that names can be retrieved by supplying an ID
-        """
-        for f_obj, f_conf in self.config["valid_objects"].iteritems():
-            self.assertTrue(
-                bool(
-                    self.api_foreman.get_name_by_id(f_conf['id'], f_obj)
-                )
-            )
-
-    def test_get_name_by_id_invalid(self):
-        """
-        Ensure that names cannot be retrieving when supplying invalid IDs
-        """
-        with self.assertRaises(SessionException):
-            for f_obj, f_conf in self.config["valid_objects"].iteritems():
-                self.assertTrue(
-                    bool(
-                        self.api_foreman.get_name_by_id(
-                            random.randint(800, 1500), f_obj
-                        )
-                    )
-                )
-
-
-
-    def test_get_id_by_name(self):
-        """
-        Ensure that IDs can be retrieved by supplying an ID
-        """
-        for f_obj, f_conf in self.config["valid_objects"].iteritems():
-            self.assertTrue(
-                bool(
-                    self.api_foreman.get_id_by_name(f_conf['name'], f_obj)
-                )
-            )
-
-    def test_get_id_by_name_invalid(self):
-        """
-        Ensure that names IDs cannot be retrieved by supplying invalid IDs
-        """
-        with self.assertRaises(SessionException):
-            for f_obj, f_conf in self.config["valid_objects"].iteritems():
-                self.api_foreman.get_id_by_name(
-                    "giertz{}".format(random.randint(800, 1500)), f_obj
-                )
-
-
-
-    def test_get_hostparams(self):
-        """
-        Ensure that host params can be retrieved
-        """
-        hostparams = self.api_foreman.get_host_params(
-            self.config["valid_objects"]["host"]["id"]
-        )
-        self.assertTrue(
-            bool(str(hostparams) != "")
-        )
-
-    def test_get_hostparams_invalid(self):
-        """
-        Ensure that host params cannot be retrieved by supplying invalid IDs
-        """
-        with self.assertRaises(SessionException):
-            hostparams = self.api_foreman.get_host_params(
-                random.randint(800, 1500)
-            )
-            self.assertTrue(
-                bool(str(hostparams) != "")
-            )
-
-    #TODO: after Spacewalk support:
-    #create/update/remove hostparam
-
-
-
-if __name__ == "__main__":
-    #start tests or die in a fire
-    if not os.path.isfile("fman_config.json"):
-        print "Please create configuration file fman_config.json!"
-        exit(1)
+    # Retrieving the ID of the previously created bookmark
+    bookmarks = json.loads(client.api_get("/bookmarks"))
+    for bookmark in bookmarks["results"]:
+        if bookmark["name"] == "ForemanAPIClientTest":
+            found_id = bookmark["id"]
+            break
     else:
-        unittest.main()
+        raise RuntimeError("Did not find bookmark!")
+
+    try:
+        yield found_id
+    finally:
+
+        try:
+            client.api_delete("/bookmarks/{}".format(found_id), "")
+        except SessionException as err:
+            if "404" not in err:
+                # 404 means demo content already removed
+                # If the error is different we want it shown.
+                raise
+
+
+def test_valid_login(client):
+    """
+    Ensure exceptions on valid logins
+    """
+    client.api_get("/architectures")
+
+
+def test_invalid_login(config):
+    """
+    Ensure exceptions on invalid logins
+    """
+    with pytest.raises(SessionException):
+        api_dummy = ForemanAPIClient(
+            logging.ERROR,
+            config["config"]["hostname"],
+            "giertz",
+            "paulapinkepank",
+            verify=False
+        )
+        # dummy call
+        api_dummy.api_get("/status")
+
+
+def test_api_get(client, bookmark_id):
+    """
+    Ensure that GET calls are working
+    """
+    bookmarks = json.loads(client.api_get("/bookmarks/{}".format(bookmark_id)))
+
+    assert bookmarks
+
+
+def test_api_put(client, bookmark_id):
+    """
+    Ensure that PUT calls are working
+    """
+    client.api_put(
+        "/bookmarks/{}".format(bookmark_id), '''
+            {
+                "bookmark": {
+                    "name": "ForemanAPIClientTest",
+                    "controller": "dashboard",
+                    "query": "architecture = i386"
+                }
+            }'''
+    )
+
+
+def test_api_delete(client, bookmark_id):
+    """
+    Ensure that DELETE calls are working
+    """
+    client.api_delete("/bookmarks/{}".format(bookmark_id), "")
+
+
+def test_api_invalid(client):
+    """
+    Ensure that dumbass calls will fail
+    """
+    with pytest.raises(SessionException):
+        client.api_get("/giertz/stdevel")
+
+
+def test_get_name_by_id(client, config):
+    """
+    Ensure that names can be retrieved by supplying an ID
+    """
+    for f_obj, f_conf in config["valid_objects"].iteritems():
+        assert client.get_name_by_id(f_conf['id'], f_obj)
+
+
+def test_get_name_by_id_invalid(client, config):
+    """
+    Ensure that names cannot be retrieving when supplying invalid IDs
+    """
+    with pytest.raises(SessionException):
+        for f_obj, f_conf in config["valid_objects"].iteritems():
+            client.get_name_by_id(random.randint(800, 1500), f_obj)
+
+
+def test_get_id_by_name(client, config):
+    """
+    Ensure that IDs can be retrieved by supplying an ID
+    """
+    for f_obj, f_conf in config["valid_objects"].iteritems():
+        assert client.get_id_by_name(f_conf['name'], f_obj)
+
+
+def test_get_id_by_name_invalid(client, config):
+    """
+    Ensure that names IDs cannot be retrieved by supplying invalid IDs
+    """
+    with pytest.raises(SessionException):
+        for f_obj, f_conf in config["valid_objects"].iteritems():
+            vm_name = "giertz{}".format(random.randint(800, 1500))
+            client.get_id_by_name(vm_name, f_obj)
+
+
+def test_get_hostparams(client, config):
+    """
+    Ensure that host params can be retrieved
+    """
+    hostparams = client.get_host_params(config["valid_objects"]["host"]["id"])
+    assert hostparams
+
+
+def test_get_hostparams_invalid(client, config):
+    """
+    Ensure that host params cannot be retrieved by supplying invalid IDs
+    """
+    with pytest.raises(SessionException):
+        client.get_host_params(random.randint(800, 1500))
