b81f13cdd9375a803f84654bc235e4de6148fcb7
Niko Wenselowski <niko.wenselowski@sva.de>
2021-03-19 14:57:45 +0100

    Test working with files from Python 2

--- tests/test_AuthContainer.py
+++ tests/test_AuthContainer.py
@@ -1,30 +1,32 @@
 import logging
 import os
+import os.path
 import pytest
-from katprep.AuthContainer import AuthContainer
+from katprep.AuthContainer import AuthContainer, ContainerException
 
 # Auth file:
 # * one encrypted
 # * one not
 
+
 @pytest.fixture
 def temp_filename(tmp_path):
-    yield os.path.join(tmp_path, 'auth-file')
+    yield os.path.join(tmp_path, "auth-file")
 
 
 @pytest.fixture
 def hostname():
-    return 'meinhost.some.domain'
+    return "meinhost.some.domain"
 
 
 @pytest.fixture
 def username():
-    return 'ingo'
+    return "ingo"
 
 
 @pytest.fixture
 def password():
-    return 'ebbelwoi'
+    return "ebbelwoi"
 
 
 def test_reading_from_unencrypted_file(temp_filename, hostname, username, password):
@@ -39,7 +41,7 @@ def test_reading_from_unencrypted_file(temp_filename, hostname, username, passwo
 
 
 def test_reading_from_encrypted_file(temp_filename, hostname, username, password):
-    key = 'hall0w3lt!'
+    key = "hall0w3lt!"
     container = AuthContainer(logging.DEBUG, temp_filename, key)
     container.add_credentials(hostname, username, password)
     container.save()
@@ -62,7 +64,7 @@ def test_retrieving_unknown_credential(temp_filename, hostname, username, passwo
     container = AuthContainer(logging.DEBUG, temp_filename)
     container.add_credentials(hostname, username, password)
 
-    assert None == container.get_credential('nothere'+ hostname)
+    assert None == container.get_credential("nothere" + hostname)
 
 
 def test_not_setting_empty_key(temp_filename):
@@ -87,3 +89,50 @@ def test_removing_credential(temp_filename, hostname, username, password):
 
     container.remove_credentials(hostname)
     assert None == container.get_credential(hostname)
+
+
+@pytest.fixture
+def unencrypted_py2_file():
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+
+    return os.path.join(test_dir, "container-py2-unencrypted.auth")
+
+
+def test_importing_unencrypted_python_2_file(unencrypted_py2_file):
+    container = AuthContainer(logging.DEBUG, unencrypted_py2_file)
+
+    hostnames = container.get_hostnames()
+    assert 1 == len(hostnames)
+
+    creds = container.get_credential(hostnames[0])
+    assert creds.username == "username"
+    assert creds.password == "password"
+
+
+@pytest.fixture
+def encrypted_py2_file():
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+
+    return os.path.join(test_dir, "container-py2.auth")
+
+
+def test_importing_encrypted_python_2_file(encrypted_py2_file):
+    filename = unencrypted_py2_file
+    container = AuthContainer(logging.DEBUG, encrypted_py2_file, "test123")
+
+    hostnames = container.get_hostnames()
+    assert 1 == len(hostnames)
+
+    creds = container.get_credential(hostnames[0])
+    assert creds.username == "username"
+    assert creds.password == "password"
+
+
+def test_import_encrypted_python_2_file_with_wrong_keyword(encrypted_py2_file):
+    container = AuthContainer(logging.DEBUG, encrypted_py2_file, "fakepassword")
+
+    hostnames = container.get_hostnames()
+    assert len(hostnames) >= 1
+
+    with pytest.raises(ContainerException):
+        container.get_credential(hostnames[0])
