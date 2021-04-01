125e2f4268dbf19e1f0cc115369fc572c2ac5a9b
Niko Wenselowski <niko.wenselowski@sva.de>
2021-04-01 17:10:58 +0200

    Better variable name

--- katprep/management/__init__.py
+++ katprep/management/__init__.py
@@ -39,23 +39,23 @@ def splitFilename(filename):
 
 
 def get_management_client(
-    virt_type, log_level, username, password, hostname, *args, **kwargs
+    management_type, log_level, username, password, hostname, *args, **kwargs
 ):
     """
     Factory for getting an appropriate management client.
     """
     # Use lazy importing here to not force our users to have every
-    # virtualisation lib installed even though they might not use them
-    if virt_type == "foreman":
+    # management lib installed even though they might not use them
+    if management_type == "foreman":
         from .foreman import ForemanAPIClient
 
         return ForemanAPIClient(log_level, host, username, password, *args, **kwargs)
-    elif virt_type == "uyuni":
+    elif management_type == "uyuni":
         from .uyuni import UyuniAPIClient
 
         return UyuniAPIClient(log_level, username, password, host, *args, **kwargs)
     else:
-        raise ValueError(f"Unknown virtualisation type {virt_type!r}")
+        raise ValueError(f"Unknown virtualisation type {management_type!r}")
 
 
 def get_virtualization_client(
