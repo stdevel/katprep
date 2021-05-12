49c0609ff948b01f911e8ecb99cdcc60ae4787f9
Christian Stankowic <christian.stankowic@sva.de>
2021-05-12 09:45:40 +0200

    move splitFilename to Uyuni class

--- katprep/management/__init__.py
+++ katprep/management/__init__.py
@@ -4,41 +4,6 @@ Clients to access various management systems.
 """
 
 
-def splitFilename(filename):
-    """
-    Pass in a standard style rpm fullname
-
-    Return a name, version, release, epoch, arch, e.g.::
-        foo-1.0-1.i386.rpm returns foo, 1.0, 1, i386
-        1:bar-9-123a.ia64.rpm returns bar, 9, 123a, 1, ia64
-
-    Proudly taken from:
-    https://github.com/rpm-software-management/
-    yum/blob/master/rpmUtils/miscutils.py
-    """
-
-    if filename[-4:] == ".rpm":
-        filename = filename[:-4]
-
-    archIndex = filename.rfind(".")
-    arch = filename[archIndex + 1 :]
-
-    relIndex = filename[:archIndex].rfind("-")
-    rel = filename[relIndex + 1 : archIndex]
-
-    verIndex = filename[:relIndex].rfind("-")
-    ver = filename[verIndex + 1 : relIndex]
-
-    epochIndex = filename.find(":")
-    if epochIndex == -1:
-        epoch = ""
-    else:
-        epoch = filename[:epochIndex]
-
-    name = filename[epochIndex + 1 : verIndex]
-    return name, ver, rel, epoch, arch
-
-
 def get_management_client(
     management_type, log_level, username, password, hostname, *args, **kwargs
 ):
--- katprep/management/uyuni.py
+++ katprep/management/uyuni.py
@@ -7,7 +7,6 @@ import ssl
 from datetime import datetime
 from xmlrpc.client import DateTime, Fault, ServerProxy
 
-from . import splitFilename
 from .base import BaseConnector
 from ..exceptions import (
     APILevelNotSupportedException,
@@ -16,6 +15,43 @@ from ..exceptions import (
     SessionException,
     SSLCertVerificationError,
 )
+
+
+def split_rpm_filename(filename):
+    """
+    Splits a standard style RPM file name into NVREA.
+    It returns a name, version, release, epoch, arch, e.g.:
+        foo-1.0-1.i386.rpm returns foo, 1.0, 1, i386
+        1:bar-9-123a.ia64.rpm returns bar, 9, 123a, 1, ia64
+
+    Proudly taken from:
+    https://github.com/rpm-software-management/
+    yum/blob/master/rpmUtils/miscutils.py
+
+    :param filename: RPM file name
+    :type filename: str
+    """
+
+    if filename[-4:] == ".rpm":
+        filename = filename[:-4]
+
+    arch_index = filename.rfind(".")
+    arch = filename[arch_index + 1 :]
+
+    rel_index = filename[:arch_index].rfind("-")
+    rel = filename[rel_index + 1 : arch_index]
+
+    ver_index = filename[:rel_index].rfind("-")
+    ver = filename[ver_index + 1 : rel_index]
+
+    epoch_index = filename.find(":")
+    if epoch_index == -1:
+        epoch = ""
+    else:
+        epoch = filename[:epoch_index]
+
+    name = filename[epoch_index + 1 : ver_index]
+    return name, ver, rel, epoch, arch
 
 
 class UyuniAPIClient(BaseConnector):
@@ -274,7 +310,7 @@ class UyuniAPIClient(BaseConnector):
         """
         try:
             # split file name
-            package_nvrea = splitFilename(file_name)
+            package_nvrea = split_rpm_filename(file_name)
             # return information
             package = self._session.packages.findByNvrea(
                 self._api_key,
