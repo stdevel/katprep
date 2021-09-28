b4fda2b90a93cf2a4dfde7e3a6e20652fba00a90
Niko Wenselowski <niko.wenselowski@sva.de>
2021-09-28 17:00:54 +0200

    Add tests for RPM splitting

---
+++ tests/test_uyuni_utilities.py
@@ -0,0 +1,15 @@
+"""
+Testing splitting the RPM filename
+"""
+
+from katprep.management.uyuni import split_rpm_filename
+
+import pytest
+
+
+@pytest.mark.parametrize("filename, expected_result", [
+    ("foo-1.0-1.i386.rpm", ("foo", "1.0", "1", "", "i386")),
+    ("1:bar-9-123a.ia64.rpm", ("bar", "9", "123a", "1", "ia64")),
+])
+def test_splitting_rpm(filename, expected_result):
+    assert expected_result == split_rpm_filename(filename)
