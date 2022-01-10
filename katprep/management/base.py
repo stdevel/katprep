d15f5da8d9bdd17ec097ac536ab529e5d2ae3f92
Christian Stankowic <info@stankowic-development.net>
2022-01-10 14:42:36 +0100

    add abstract methods for pre/post scripts

--- katprep/management/base.py
+++ katprep/management/base.py
@@ -3,25 +3,60 @@ Basic management client
 """
 
 from abc import abstractmethod
-from typing import List
 
 from ..connector import BaseConnector
 
 
 class ManagementClient(BaseConnector):
+    """
+    Management client class stub
+    """
+
     @abstractmethod
+    def install_pre_script(self, host):
+        """
+        Runs the installation pre-script
+        """
+
+    @abstractmethod
+    def install_post_script(self, host):
+        """
+        Runs the installation post-script
+        """
+
+    @abstractmethod
+    def install_plain_patches(self, host):
+        """
+        Installs patches without running the pre-script
+        """
+
+    @abstractmethod
+    def install_plain_upgrades(self, host):
+        """
+        Installs upgrades without running the pre-script
+        """
+
     def install_patches(self, host):
         """
         Apply the patches with the given errata_ids on the given host.
         """
+        if host.pre_script:
+            self.install_pre_script(host)
+        self.install_plain_patches(host)
+        if host.post_script:
+            self.install_post_script(host)
 
-    @abstractmethod
     def install_upgrades(self, host):
         """
         Upgrade all packages on the given host.
 
-        This does include ugrades that are not part of an errata.
+        This does include upgrades that are not part of an errata.
         """
+        if host.pre_script:
+            self.install_pre_script(host)
+        self.install_plain_upgrades(host)
+        if host.post_script:
+            self.install_post_script(host)
 
     @abstractmethod
     def reboot_host(self, host):
